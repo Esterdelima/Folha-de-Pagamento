a1e4e8f941ace907c8e3be1d2075577ae1233c03
Esterdelima <ester_delima@hotmail.com>
2021-08-11 10:25:34 -0300

    add func. 1 a 6

---
+++ folha.py
@@ -0,0 +1,15 @@
+class Folha():
+
+    teste= ''
+    
+    def __init__(self,teste):
+        pass
+        self.teste = teste
+        
+
+    def folhaPagamento(self):
+        print('entrei na folha')
+
+
+
+#print(self.salario)
